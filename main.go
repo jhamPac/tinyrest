1ece5b57270e3b4a2fb1f5ccf8d14ae6aea1c820
jhamPac <cloud@kevia.me>
2020-07-20 19:01:08 -0700

    show function for entry

--- main.go
+++ main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"log"
+	"net/http"
 	"sync"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 type store struct {
@@ -19,3 +24,31 @@ var (
 		m:    sync.RWMutex{},
 	}
 )
+
+func main() {
+	flag.Parse()
+
+	r := httprouter.New()
+	r.GET("/entry/:key", show)
+	r.GET("/list", show)
+	r.PUT("/entry/:key/:value", update)
+	err := http.ListenAndServe(*addr, r)
+
+	if err != nil {
+		log.Fatal("server failed", err)
+	}
+}
+
+func show(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	k := p.ByName("key")
+
+	if k == "" {
+		s.m.RLock()
+		fmt.Fprintf(w, "read list: %v", s.data)
+		s.m.RUnlock()
+		return
+	}
+	s.m.RLock()
+	fmt.Fprintf(w, "read entry: s.data[%s] = %s", k, s.data[k])
+	s.m.RUnlock()
+}
